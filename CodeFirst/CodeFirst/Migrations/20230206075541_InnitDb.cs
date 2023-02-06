7fa6838e6dc6f5fcf97a0cd7c8a8059915517e8c
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2023-02-06 20:02:15 +0700

    update
    hoat

---
+++ CodeFirst/CodeFirst/Migrations/20230206075541_InnitDb.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StrongData.Migrations
+{
+    public partial class InnitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Capacity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.RoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "RoomId", "Capacity", "RoomName" },
+                values: new object[] { 1, 1, "B8" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Address", "Gender", "RoomId", "Status", "StudentName" },
+                values: new object[] { 1, "nam dinh", 1, 1, 1, "mai huy hoat" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_RoomId",
+                table: "Students",
+                column: "RoomId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
---
+++ CodeFirst/CodeFirst/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+      
+        public string? RoomName { get; set; }
+        
+        public int Capacity { get; set; }
+        //navigation prorperty
+        public virtual List<Student>? Students { get; set; }
+    }
+}
