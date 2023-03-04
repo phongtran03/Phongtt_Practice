100e23e4bad712c263369c528f2363d6de8155f8
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2023-03-04 08:41:41 +0700

    up
    up

---
+++ Exam/Exam/Exam/Migrations/20230220123614_initDb.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exam.Migrations
+{
+    public partial class initDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Picture = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: true),
+                    SupplierID = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoryID = table.Column<string>(type: "TEXT", nullable: true),
+                    QuantityPerUnit = table.Column<double>(type: "REAL", nullable: false),
+                    UnitPrice = table.Column<double>(type: "REAL", nullable: false),
+                    UnitslnStock = table.Column<double>(type: "REAL", nullable: false),
+                    UnnitsOnOrder = table.Column<double>(type: "REAL", nullable: false),
+                    ReorderLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    Discontinued = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryProduct",
+                columns: table => new
+                {
+                    CategorieCategoryID = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductsProductID = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryProduct", x => new { x.CategorieCategoryID, x.ProductsProductID });
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Categories_CategorieCategoryID",
+                        column: x => x.CategorieCategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Products_ProductsProductID",
+                        column: x => x.ProductsProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName", "Description", "Picture" },
+                values: new object[] { "SK1", "Mobile Phone", "The Phone for every one", "image1.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryName", "Description", "Picture" },
+                values: new object[] { "SK2", "Book", "Book for Student", "image2.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductID", "CategoryID", "Discontinued", "ProductName", "QuantityPerUnit", "ReorderLevel", "SupplierID", "UnitPrice", "UnitslnStock", "UnnitsOnOrder" },
+                values: new object[] { "QW1", "SK1", 5.5999999999999996, "Iphone 15", 1.0, 1, "HJ2", 23.440000000000001, 0.13, 3.0 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductID", "CategoryID", "Discontinued", "ProductName", "QuantityPerUnit", "ReorderLevel", "SupplierID", "UnitPrice", "UnitslnStock", "UnnitsOnOrder" },
+                values: new object[] { "QW2", "SK2", 5.5999999999999996, "JAVA book", 2.0, 1, "HJ3", 20.440000000000001, 0.13, 3.0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryProduct_ProductsProductID",
+                table: "CategoryProduct",
+                column: "ProductsProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryProduct");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- Practice/Practice/Models/DatabaseContext.cs
+++ Practice/Practice/Models/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pratice.Models;
 
 namespace Practice.Models
 {
@@ -7,97 +8,53 @@ namespace Practice.Models
         public DatabaseContext(DbContextOptions options) : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            modelBuilder.Entity<Student>().HasData(new Student
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category
             {
-                StudentId = 1,
-                Name = "nguyen van B",
-                DateOfBirth = new DateTime(2001, 04, 20),
-                Email = "hoatdfk2001@gmail.com",
-                Address = "Nam Dinh"
-
+                CategoryID = "SK1",
+                CategoryName = "Mobile Phone",
+                Description = "The Phone for every one",
+                Picture = "image1.jpg"
+            });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryID = "SK2",
+                CategoryName = "Book",
+                Description = "Book for Student",
+                Picture = "image2.jpg"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = "QW1",
+                ProductName = "Iphone 15",
+                SupplierID = "HJ2",
+                CategoryID = "SK1",
+                QuantityPerUnit = 1,
+                UnitPrice = 23.44,
+                UnitslnStock = 0.13,
+                UnnitsOnOrder = 3,
+                ReorderLevel = 1,
+                Discontinued = 5.6
 
             });
-            modelBuilder.Entity<Student>().HasData(new Student
+            modelBuilder.Entity<Product>().HasData(new Product
             {
-                StudentId = 2,
-                Name = "nguyen van A",
-                DateOfBirth = new DateTime(2001, 04, 21),
-                Email = "hoatdfk2001@gmail.com",
-                Address = "Nam Dinh"
-
+                ProductID = "QW2",
+                ProductName = "JAVA book",
+                SupplierID = "HJ3",
+                CategoryID = "SK2",
+                QuantityPerUnit = 2,
+                UnitPrice = 20.44,
+                UnitslnStock = 0.13,
+                UnnitsOnOrder = 3,
+                ReorderLevel = 1,
+                Discontinued = 5.6
 
             });
-            modelBuilder.Entity<Student>().HasData(new Student
-            {
-                StudentId = 3,
-                Name = "nguyen van C",
-                DateOfBirth = new DateTime(2001, 04, 22),
-                Email = "hoatdfk2001@gmail.com",
-                Address = "Nam Dinh"
-
-
-            });
-            modelBuilder.Entity<Subject>().HasData(
-                new Subject
-                {
-                    SubjectId = 1,
-                    SubjectName = "C start it program",
-                    SubjectCode = "K203",
-                    Description = "this is a language",
-                    StartDate = new DateTime(2022, 3, 28),
-                    EndDate = new DateTime(2023, 1, 28)
-                }
-                );
-            modelBuilder.Entity<Subject>().HasData(
-                new Subject
-                {
-                    SubjectId = 2,
-                    SubjectName = "PHP program",
-                    SubjectCode = "K204",
-                    Description = "this is a language",
-                    StartDate = new DateTime(2022, 3, 28),
-                    EndDate = new DateTime(2023, 1, 28)
-                }
-                );
-            modelBuilder.Entity<Subject>().HasData(
-                new Subject
-                {
-                    SubjectId = 3,
-                    SubjectName = "JAVA  program",
-                    SubjectCode = "K205",
-                    Description = "this is a language",
-                    StartDate = new DateTime(2022, 3, 28),
-                    EndDate = new DateTime(2023, 1, 28)
-                }
-                );
-            modelBuilder.Entity<Exam>().HasData(
-                new Exam
-                {
-                    ExamId = 1,
-                    Score = 5,
-                    StudentId = 1,
-                    SubjectId = 1
-                });
-            modelBuilder.Entity<Exam>().HasData(
-               new Exam
-               {
-                   ExamId = 2,
-                   Score = 8,
-                   StudentId = 2,
-                   SubjectId = 2
-               });
-            modelBuilder.Entity<Exam>().HasData(
-               new Exam
-               {
-                   ExamId = 3,
-                   Score = 10,
-                   StudentId = 3,
-                   SubjectId = 3
-               });
         }
-        public DbSet<Exam> Exams { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Subject> Subjects { get; set; }    
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
